64932e6e5092776a36659b0df15e30e65ca7fe3e
Milan Sony <milanfrom2001@gmail.com>
2023-03-22 14:12:56 +0530

    youtube vdo downloader code witten

---
+++ yt2.py
@@ -0,0 +1,48 @@
+from pytube import YouTube
+from pytube.cli import on_progress #this module contains the built in progress bar. 
+from sys import argv
+
+def dwnldavideo():
+  # Get link from commandline
+  # Eg: python ytdwnld.py "https://youtu.be/m7Bc3pLyij0"
+  ytlink = input("Youtube video link: ")
+
+  # Creating an object of youtube
+  ytobj = YouTube(ytlink, on_progress_callback = on_progress)
+
+  # Print some description about the youtube video
+  print("Title:", ytobj.title)
+  print("Creator:", ytobj.author)
+
+  # Download the video
+  ytvideo = ytobj.streams.get_highest_resolution()
+  ytvideo.download(r'C:\Users\MILAN\downloads')
+  print("\n")
+  print("Download Complete")
+  print("\n")
+
+def dwnldaplaylist():
+  print("playlist")
+
+def dwnldachannel():
+  print("channel")
+
+def main():
+  while True:
+    print("Enter your choice")
+    print("1. Download a youtube video \n2. Download a youtube playlist \n3. Download all of the videos created by a channel \n4. Exit")
+    choice = input("Please enter the choice: ")
+
+    if (choice == '1'):
+      dwnldavideo()
+    elif (choice == '2'):
+      dwnldaplaylist()
+    elif (choice == '3'):
+      dwnldachannel()
+    elif (choice == '4'):
+      print("You have choose exit | Type: python ytdwnld.py to run again")
+      exit()
+    else:
+      print("Invalid choice. Please try again")
+
+main()
---
+++ ytdwnld.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+from pytube.cli import on_progress #this module contains the built in progress bar. 
+from sys import argv
+
+# Get link from commandline
+# Eg: python ytdwnld.py "https://youtu.be/m7Bc3pLyij0"
+ytlink = argv[1]
+
+# Creating an object of youtube
+ytobj = YouTube(ytlink, on_progress_callback = on_progress)
+
+# Print some description about the youtube video
+print("Title:", ytobj.title)
+print("Creator:", ytobj.author)
+
+# Download the video
+ytvideo = ytobj.streams.get_highest_resolution()
+ytvideo.download(r'C:\Users\MILAN\downloads')
+print("\n")
+print("Download Complete")
+
+
+"""Documentations
+pytube : https://pytube.io/en/latest/user/quickstart.html
+progress bar : https://stackoverflow.com/a/68419849
+"""
